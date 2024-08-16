d5239c5db7701fcb9733a7968a1014b99117a577
BasavarajMuttagi <basavaraj2770@gmail.com>
2024-08-16 16:25:57 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { RouterProvider } from "react-router-dom";
+import "./App.css";
+import routes from "./routes/routes";
+import { Toaster } from "react-hot-toast";
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={routes} />
+      <Toaster />
+    </>
+  );
+}
+
+export default App;
