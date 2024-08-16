77caab40106e78a1ef5e1b715a2125abbaafd7d9
BasavarajMuttagi <basavaraj2770@gmail.com>
2024-08-16 16:52:47 +0530

    create store

---
+++ src/store.ts
@@ -0,0 +1,47 @@
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+const storageModule = {
+  name: "addictive-media-storage",
+  storage: createJSONStorage(() => sessionStorage),
+};
+
+type store = {
+  token: string;
+  displayName: string;
+  email: string;
+  setToken: (newToken: string) => void;
+  setDisplayName: (name: string) => void;
+  setEmail: (email: string) => void;
+  logout: () => void;
+  reset: () => void;
+};
+
+const useAddictiveStore = create<store>()(
+  persist(
+    (set) => ({
+      token: "",
+      displayName: "",
+      email: "",
+      setToken: (newToken) => set(() => ({ token: newToken })),
+      setDisplayName: (name: string) => set(() => ({ displayName: name })),
+      setEmail: (email: string) => set(() => ({ email })),
+      logout: () => {
+        set(() => ({
+          token: "",
+          displayName: "",
+          email: "",
+        }));
+      },
+      reset: () => {
+        set(() => ({
+          token: "",
+          displayName: "",
+          email: "",
+        }));
+      },
+    }),
+    storageModule,
+  ),
+);
+export default useAddictiveStore;
