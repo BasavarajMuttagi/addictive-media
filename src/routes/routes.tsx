1f162ab08258ce02db270a00c5aa7b8372a89966
BasavarajMuttagi <basavaraj2770@gmail.com>
2024-08-16 19:28:00 +0530

    create login & signup forms and integrate

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,10 +1,27 @@
 import { createBrowserRouter } from "react-router-dom";
 import PageNotFound from "../pages/PageNotFound";
+import AuthLayout from "../layouts/AuthLayout";
+import Public from "./Public";
+import SignUp from "../components/SignUp";
+import Login from "../components/Login";
 
 const routes = createBrowserRouter([
   {
-    path: "/",
-    element: <></>,
+    element: (
+      <AuthLayout>
+        <Public />
+      </AuthLayout>
+    ),
+    children: [
+      {
+        path: "/signup",
+        element: <SignUp />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
   },
   {
     path: "*",
