b6d6e61f38139a65b1bdbc7260335ade2f7c363f
BasavarajMuttagi <basavaraj2770@gmail.com>
2024-08-19 15:11:17 +0530

    fix format

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -35,7 +35,9 @@ const Login = () => {
       location.reload();
     } catch (error) {
       if (isAxiosError(error)) {
-        return toast.error(error.response?.data.message||"Something went wrong");
+        return toast.error(
+          error.response?.data.message || "Something went wrong",
+        );
       }
       return toast.error("Something went wrong");
     } finally {
